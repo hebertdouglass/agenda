65ad6153eef16aba8ead787b07e33ccb452863a6
hebertdouglass <hebertdouglas735@gmail.com>
2022-06-02 19:41:43 -0300

    Adicionado funções de alteração e exclusão de dados

--- agenda/core/models.py
+++ agenda/core/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
+    #local = models.CharField(max_length=70, blank=True, null=True)
     descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True)
@@ -18,3 +19,6 @@ class Evento(models.Model):
 
     def get_data_evento(self):
         return self.data_evento.strftime('%d/%m/%Y %H:%M Horas')
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
